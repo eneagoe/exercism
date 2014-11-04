704b66613f24fd7a76bfffa6fff3e3642f0bb07f
eugen neagoe <eneagoe@gmail.com>
2014-11-04 21:24:28 +0200

    simplified return value

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -4,7 +4,6 @@ class Raindrops
     str << "Pling" if n % 3 == 0
     str << "Plang" if n % 5 == 0
     str << "Plong" if n % 7 == 0
-    str = n.to_s if str.empty?
-    str
+    str.empty? ? n.to_s : str
   end
 end
